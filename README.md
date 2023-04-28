f7c6b76bb9f10230663c0cae03b882d44d0b0c32
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-04-28 13:33:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# online-clothing
+ React |Express |Firebase |Node-sass |Redux-persist |Reselect |Redux-saga |styled-components |stripe |PWA |Performance optimization etc. 
+ 
+ view app: 
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const bodParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
+const enforce =  require("express-sslify");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 4400;
+
+app.use(compression());
+app.use(bodParser.json());
+app.use(bodParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`server running on port :${port}`);
+});
+
+// use for PWA to get the service-worker file inside build
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
+
+
+app.post("/payment", (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: "usd"
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if(stripeErr) {
+            res.status(500).send({ error: stripeErr });
+        }else {
+            res.status(200).send({ success: stripeRes });
+        }
+    })
+})
