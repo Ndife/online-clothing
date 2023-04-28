db978279d973d92457a76cfad25283b74d6ffc61
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-04-28 17:15:01 +0100

    update firebase

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -8,17 +7,20 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
-// require('dotenv').config()
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
-        <App />
+        <App  tab="home" />
       </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
 );
 
 serviceWorker.register();
