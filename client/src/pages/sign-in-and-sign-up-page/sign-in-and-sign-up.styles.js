5987c10f69ea52d07595425d96767a7f4fe161d5
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-10-17 07:50:39 +0100

    fix styles error

--- client/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.styles.js
+++ client/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SignInAndSignUpContainer = styled.div`
-  width: 850px;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
