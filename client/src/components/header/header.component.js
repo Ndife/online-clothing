0e9b332b089432e67b97c39a17b90a94f6f14185
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2024-10-16 12:05:33 +0100

    add mobile dropdown icon

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -9,41 +9,91 @@ import { selectCurrentUser } from "../../redux/user/user.selector";
 import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as HamburgerIcon } from "../../assets/hamburger.svg";
 import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
+  OptionsContainerMobile,
   OptionLink,
+  MobileMenuIcon,
 } from "./header.styles";
 
-const Header = ({ currentUser, hidden, signOutStart }) => (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to="/shop">SHOP</OptionLink>
-      <OptionLink to="/contact">CONTACT</OptionLink>
-      {currentUser ? (
-        <OptionLink as="div" onClick={signOutStart}>
-          SIGN OUT
-        </OptionLink>
-      ) : (
-        <OptionLink to="/signin">SIGN IN</OptionLink>
+const Header = ({ currentUser, hidden, signOutStart }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => setIsDropdownOpen((prevState) => !prevState);
+
+  const closeDropdown = () => setIsDropdownOpen(false);
+
+  // Close dropdown on outside click
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo />
+      </LogoContainer>
+
+      <MobileMenuIcon>
+        <CartIcon />
+        <HamburgerIcon onClick={toggleDropdown} />
+      </MobileMenuIcon>
+
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">CONTACT</OptionLink>
+        {currentUser ? (
+          <OptionLink as="div" onClick={signOutStart}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to="/signin">SIGN IN</OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+
+      {isDropdownOpen && (
+        <OptionsContainerMobile ref={dropdownRef}>
+          <OptionLink to="/shop" onClick={closeDropdown}>SHOP</OptionLink>
+          <OptionLink to="/contact" onClick={closeDropdown}>CONTACT</OptionLink>
+          {currentUser ? (
+            <OptionLink as="div" onClick={() => {
+              signOutStart();
+              closeDropdown();
+            }}>
+              SIGN OUT
+            </OptionLink>
+          ) : (
+            <OptionLink to="/signin" onClick={closeDropdown}>SIGN IN</OptionLink>
+          )}
+        </OptionsContainerMobile>
       )}
-      <CartIcon />
-    </OptionsContainer>
-    {hidden ? null : <CartDropdown />}
-  </HeaderContainer>
-);
+
+      {!hidden && <CartDropdown />}
+    </HeaderContainer>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   hidden: selectCartHidden,
 });
 
-const mapDispatchToProps = dispatch => ({
-  signOutStart: () => dispatch(signOutStart())
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- client/src/components/header/header.styles.js
+++ client/src/components/header/header.styles.js
@@ -8,11 +8,11 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   margin-bottom: 25px;
 
-  @media screen and (max-width: 800px){
+  @media screen and (max-width: 800px) {
     height: 60px;
     padding: 10px;
     margin-bottom: 20px;
-  }  
+  }
 `;
 
 export const LogoContainer = styled(Link)`
@@ -20,10 +20,10 @@ export const LogoContainer = styled(Link)`
   width: 70px;
   padding: 25px;
 
-  @media screen and (max-width: 800px){
+  @media screen and (max-width: 800px) {
     width: 50px;
     padding: 0;
-  }  
+  }
 `;
 
 export const OptionsContainer = styled.div`
@@ -33,12 +33,53 @@ export const OptionsContainer = styled.div`
   align-items: center;
   justify-content: flex-end;
 
-  @media screen and (max-width: 800px){
-    width: 90%;
+  @media screen and (max-width: 800px) {
+    display: none;
+  }
+`;
+
+export const OptionsContainerMobile = styled.div`
+  display: none;
+
+  @media screen and (max-width: 800px) {
+    margin: 2%;
+    height: 100%vh;
+    display: flex;
+    width: 100%;
+    position: absolute;
+    top: 60px;
+    right: 0;
+    background-color: white;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    z-index: 5;
   }
 `;
 
 export const OptionLink = styled(Link)`
-  padding: 10px 15px;
+  padding: 2% 15px;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(211, 211, 211, 0.5); /* LightGray with 50% transparency */
+  }
+`;
+
+export const MobileMenuIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 800px) {
+    display: flex;
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    cursor: pointer;
+    align-items: center;
+  }
 `;
